60cbaf022ede68bc6c4ae991485c9307136d1ace
Lindsey Saari <lisaari@nmu.edu>
2014-05-28 11:20:59 -0400

    Initialize repository

---
+++ config/application.rb
@@ -0,0 +1,25 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module FirstApp
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+    
+    config.secret_key_base = '605cfb1bebc2c4cf922782e1fbc971216aae504b2b814c307c744b931509a79042ae3ebcca95a3ef351dfcfb63fab40b0131912beeaf28e6a41e34d12be3de9b'
+  end
+end
